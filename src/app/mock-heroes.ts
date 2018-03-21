77ffb12a46c1ece5ddd9edd879803a0811014262
Maharjan <dxm40@deepen-mbp.acuitylightinggroup.com>
2018-03-21 15:34:47 -0700

    added Heroes list and displayed

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,44 @@
+import { Hero } from '../app/hero';
+
+export const HEROES: Hero[] = [
+    {
+        id:     0,
+        name:   'Windranger'
+    },
+    {
+        id:     1,
+        name:   'Axe'
+    },
+    {
+        id:     2,
+        name:   'Shadow Fiend'
+    },
+    {
+        id:     3,
+        name:   'Wraith King'
+    },
+    {
+        id:     4,
+        name:   'Monkey King'
+    },
+    {
+        id:     5,
+        name:   'Phantom Lancer'
+    },
+    {
+        id:     6,
+        name:   'Bane'
+    },
+    {
+        id:     7,
+        name:   'Clinkz'
+    },
+    {
+        id:     8,
+        name:   'Shadow Shaman'
+    },
+    {
+        id:     9,
+        name:   'Luna'
+    }
+];
