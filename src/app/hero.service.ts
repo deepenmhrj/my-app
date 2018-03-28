f16a74c97fcd32f940eaf6a0d2f4c322b321b2fb
Maharjan <dxm40@deepen-mbp.acuitylightinggroup.com>
2018-03-28 14:25:23 -0700

    -added HeroService, -provided HeroService via AppModule, -updated heroes.component class to use HeroService

---
+++ src/app/hero.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Hero } from '../app/hero';
+import { HEROES } from '../app/mock-heroes';
+
+@Injectable()
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
+
+}
